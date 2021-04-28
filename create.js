a632eaaa03133e449af0a3959e742d8c5bc414fe
Bhasin Neeraj <nkbhasin28@gmail.com>
2021-04-28 11:39:14 -0400

    adding serverless apis

---
+++ create.js
@@ -0,0 +1,22 @@
+import * as uuid from "uuid";
+import handler from "./libs/handler-lib";
+import dynamoDB from "./libs/dynamodb-lib";
+
+export const main = handler(async (event, context) => {
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      //attributes of the item to be created
+      userId: "123",
+      noteId: uuid.v1(),
+      content: data.content,
+      attachment: data.attachment,
+      createdAt: Date.now(),
+    },
+  };
+
+  await dynamoDB.put(params).promise();
+  return params.Item;
+});
